7de585e2852769ce0b46f87db4e8a6f3a45349d7
Jairo Batista <bebaza.america@gmail.com>
2025-06-02 22:49:59 -0500

    different image address App.js

--- src/App.js
+++ src/App.js
@@ -219,7 +219,7 @@ function App() {
                 <div className="relative">
                   <div className="relative rounded-3xl overflow-hidden shadow-2xl">
                     <img 
-                      src="/jairo-chuy-spacex.jpg"
+                      src="https://imgur.com/a/WRjjCvZ"
                       alt="Jairo with Chuy at SpaceX"
                       className="w-full h-auto object-cover"
                       style={{ minHeight: '400px' }}
